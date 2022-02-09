acdd59882277d56d1923d28b345d69b3531dade2
soundharvs <soundharvs2001@gmail.com>
2022-02-09 14:03:57 +0530

    ready to deploy

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,29 +1,41 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create({channel: "RoomChannel", room_id: 2}, {
-  connected() {
-    // Called when the subscription is ready for use on the server
-    console.log("connected...")
-  },
+document.addEventListener('turboliks:load', () => {
+  const room_element = document.getElementById('room-id');
+  const room_id = Number(room_element.getAttribute('data-room-id'));
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+  consumer.log(consumer.subscriptions)
 
-  received(data) {
+  consumer.subscriptions.subscriptions.forEach((subscription) => {
+    consumer.subscriptions.remove(subscription)
+  })
 
-    const element = document.getElementById('user-id');
-    const user_id = Number(element.getAttribute('data-user-id')); 
-
-    let html;
-
-    if(user_id === data.message.user_id){
-      html = data.mine
-    }else{
-      html = data.theirs
+  consumer.subscriptions.create({channel: "RoomChannel", room_id: room_id}, {
+    connected() {
+      // Called when the subscription is ready for use on the server
+      console.log("connected to" + room_id)
+    },
+  
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
+  
+    received(data) {
+  
+      const element = document.getElementById('user-id');
+      const user_id = Number(user_element.getAttribute('data-user-id')); 
+  
+      let html;
+  
+      if(user_id === data.message.user_id){
+        html = data.mine
+      }else{
+        html = data.theirs
+      }
+  
+      const messageContainer = document.getElementById('messages')
+      messageContainer.innerHTML = messageContainer.innerHTML + html
     }
+  });
+})
 
-    const messageContainer = document.getElementById('messages')
-    messageContainer.innerHTML = messageContainer.innerHTML + html
-  }
-});
